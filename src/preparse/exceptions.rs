89b08455bdbe7058819e0270fd1e54ed6a25a4f4
Alisa Sireneva <me@purplesyringa.moe>
2025-12-02 17:04:36 +0300

    Fix EH coalescing

--- src/preparse/exceptions.rs
+++ src/preparse/exceptions.rs
@@ -1,8 +1,5 @@
 use super::{CatchHandler, basic_blocks::ExceptionHandler};
-use crate::ast::Str;
 use crate::utils::merge_overlapping_ranges;
-use core::ops::Range;
-use rustc_hash::FxHashMap;
 
 pub fn coalesce_exception_handlers(
     exception_handlers: Vec<ExceptionHandler<'_>>,
@@ -11,21 +8,23 @@ pub fn coalesce_exception_handlers(
     // correspond to the same `catch` block. This is not *guaranteed* to be true, but it's true for
     // javac and it's as intuitive as it gets. If we're mistaken, the worst that can happen is we
     // get a slightly worse output.
-    let mut by_target_class: FxHashMap<(usize, Option<Str<'_>>), Vec<Range<usize>>> =
-        FxHashMap::default();
-    for handler in exception_handlers {
-        by_target_class
-            .entry((handler.target, handler.class))
-            .or_default()
-            .push(handler.active_range);
-    }
 
-    by_target_class
-        .into_iter()
-        .map(|((target, class), mut active_ranges)| {
+    // We can't just collect handlers into a hash map here because the order of exception handlers
+    // matters, so we only look for consecutive matches. javac always generates such entries
+    // consecutively.
+    exception_handlers
+        .chunk_by(|a, b| (a.target, a.class) == (b.target, b.class))
+        .map(|chunk| {
+            let ExceptionHandler { target, class, .. } = chunk[0];
+
+            let mut active_ranges: Vec<_> = chunk
+                .iter()
+                .map(|handler| handler.active_range.clone())
+                .collect();
             active_ranges.sort_unstable_by_key(|range| range.start);
             merge_overlapping_ranges(&mut active_ranges);
             assert!(!active_ranges.is_empty(), "no active ranges for `catch`");
+
             CatchHandler {
                 active_ranges,
                 target,
